d4daeef990c2a968f1bb7e8c623eceec25c222ed
Esko-Kalervo Salaka <esko.salaka@gmail.com>
2022-10-30 19:49:04 +0200

    added a bit more space in side menu

--- src/app/common/nav/nav.component.ts
+++ src/app/common/nav/nav.component.ts
@@ -32,7 +32,7 @@ import { BarsService } from 'src/app/services/bars.service'
       }
 
       .sidenav {
-        width: 150px;
+        width: 160px;
         background-color: transparent;
         border-width: 0;
         margin-top: 56px;
