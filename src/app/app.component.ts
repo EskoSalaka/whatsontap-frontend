bc005d3fab2d606f497f4bee16efb9bbb22e54d2
Esko-Kalervo Salaka <esko.salaka@gmail.com>
2022-12-03 13:35:28 +0200

    added a loading animation for app startup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,34 +1,49 @@
+import { ThisReceiver } from '@angular/compiler'
 import { Component } from '@angular/core'
 import { NgxSpinnerService } from 'ngx-spinner'
+import { tap } from 'rxjs'
 import { ApiService } from './services/api.service'
 import { BarsService } from './services/bars.service'
 
 @Component({
   selector: 'app-root',
   template: `
-    <app-nav></app-nav>
+    <ngx-spinner
+      bdColor="rgba(0, 0, 0, 0.8)"
+      size="medium"
+      color="#fff"
+      type="ball-8bits"
+      [fullScreen]="true">
+      <p style="color: white">Loading...</p>
+    </ngx-spinner>
+
+    <app-nav *ngIf="!loading"></app-nav>
   `,
 })
 export class AppComponent {
   title = 'whatsontap'
-  loading = true
+  loading: boolean
 
   constructor(
     private api: ApiService,
-    private spin: NgxSpinnerService,
-    private barService: BarsService
+    public spin: NgxSpinnerService,
+    public barService: BarsService
   ) {}
 
   ngOnInit(): void {
+    this.spin.show()
+    this.loading = true
     this.api.getBars().subscribe({
       next: (data) => {
         this.barService.setBars(data)
+        this.spin.hide()
+        this.loading = false
       },
       error: (err) => {
         console.log('error', err)
+        this.spin.hide()
+        this.loading = false
       },
     })
-
-    //this.spin.show()
   }
 }
